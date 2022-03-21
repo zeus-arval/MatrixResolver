24a23c6bee0de7734cad08fc3736beabe615b5cc
Artur Valdna <artur.valdna@playtech.com>
2022-03-21 11:12:39 +0200

    Added some code to main method

--- MatrixResolver/Program.cs
+++ MatrixResolver/Program.cs
@@ -6,6 +6,13 @@ namespace MatrixResolver
     {
         static void Main(string[] args)
         {
+            int rows = 3;
+            int columns = 3;
+            int[][] array;
+            MatrixGenerator.GenerateRandomJaggedArray(rows, columns, out array);
+
+            Matrix matrix = new Matrix(array);
+            matrix.PrintMatrix();
         }
     }
 }
