2a02a13a4023badd5c91c7aaa409f5501e1a4702
Artur Valdna <artur.valdna@playtech.com>
2022-03-22 16:38:09 +0200

    Added Multiplication for matrix

---
+++ MatrixResolver/MatrixSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixResolver
+{
+    public static class MatrixSolver
+    {
+
+        public static Matrix Multiply(this Matrix matrix, in Matrix targetMatrix)
+        {
+            if(matrix.ColumnsCount != targetMatrix.RowsCount)
+            {
+                throw new Exception("Amount of first matrix' columns is not equal to amount of second matrix' rows");
+            }
+
+            int[][] jaggedArray = new int[targetMatrix.RowsCount][];
+
+            for (int matrixY = 0; matrixY < matrix.RowsCount; matrixY++)
+            {
+                int[] newArray = new int[targetMatrix.ColumnsCount];
+
+                for (int targetX = 0; targetX < targetMatrix.ColumnsCount; targetX++)
+                {
+                    int number = 0;
+
+                    for (int x = 0; x < matrix.ColumnsCount; x++)
+                    {
+                        number += matrix.MatrixValue[matrixY][x] * targetMatrix.MatrixValue[x][targetX];
+                    }
+
+                    newArray[targetX] = number;
+                }
+
+                jaggedArray[matrixY] = newArray;
+            }
+
+            return new Matrix(jaggedArray);
+        }
+    }
+}
--- MatrixResolver/Program.cs
+++ MatrixResolver/Program.cs
@@ -6,12 +6,43 @@ namespace MatrixResolver
     {
         static void Main(string[] args)
         {
-            int rows = 3;
-            int columns = 3;
-            Matrix matrix;
-            MatrixGenerator.GenerateRandomMatrix(rows, columns, out matrix);
+            const int rows = 3;
+            const int columns = 3;
+            //Matrix matrix;
+            //MatrixGenerator.GenerateRandomMatrix(rows, columns, out matrix);
+
+            Matrix matrix = new(new int[3][]{
+                new int[]{ 5, 8, -4},
+                new int[]{ 6, 9, -5},
+                new int[]{ 4, 7, -3}
+            });
+
+            Matrix target = new(new int[3][]{
+                new int[]{ 3, 2, 5},
+                new int[]{ 4, -1, 3},
+                new int[]{ 9, 6, 5}
+            });
+
+            //Matrix matrix = new(new int[rows][]{
+            //    new int[]{ 5, 8, -4 },
+            //    new int[]{ 6, 9, -5 },
+            //    new int[]{ 4, 7, -3 }
+            //});
+
+            //Matrix target = new(new int[columns][]{
+            //    new int[]{ 2 },
+            //    new int[]{ -3 },
+            //    new int[]{ 1 },
+            //});
 
             matrix.PrintMatrix();
+            Console.WriteLine();
+            target.PrintMatrix();
+            Console.WriteLine();
+
+            Matrix result = MatrixSolver.Multiply(matrix, target);
+
+            result.PrintMatrix();
         }
     }
 }
